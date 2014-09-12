66e0f09b6af57e2f6667dfaf7dac1d7dda46a974
liluo <mwumli@hotmail.com>
2014-09-12 15:26:02 +0800

    new file:config.sh, 增添安装git和下载vundle到~/.vim/bundle/Vundle.vim

---
+++ config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+# 安装git
+sudo apt-get install git
+# 下载Vundle到~/.vim/bundle/Vundle.vim
+mkdir -p ~/.vim/bundle/ &&
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+
+
+
+
+
+
+mv
