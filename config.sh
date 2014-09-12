96106e6be19b1d42ee584309f375d3e7ed7cae44
liluo <mwumli@hotmail.com>
2014-09-12 15:29:30 +0800

    config.sh:移动.vimrc到你的home目录

--- config.sh
+++ config.sh
@@ -5,11 +5,9 @@ sudo apt-get install git
 # 下载Vundle到~/.vim/bundle/Vundle.vim
 mkdir -p ~/.vim/bundle/ &&
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mv vim/.vimrc ~/.vimrc		"移动.vimrc到你的home目录
 
 
 
 
 
-
-
-mv
