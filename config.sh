86f4bd7c57fd9710cb178766c45de84770621641
liluo <mwumli@hotmail.com>
2014-09-16 17:03:57 +0800

    install vim

--- config.sh
+++ config.sh
@@ -5,11 +5,13 @@ sudo apt-get install git
 # 下载Vundle到~/.vim/bundle/Vundle.vim
 mkdir -p ~/.vim/bundle/ &&
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# vim配色插件
 mkdir -p ~/.vim/colors &&
 cp vim/colors/molokai.vim ~/.vim/colors/
 
-mv vim/.vimrc ~/.vimrc		#移动.vimrc到你的home目录
-
+cp vim/.vimrc ~/.vimrc		#移动.vimrc到你的home目录
+# 安装vim
+sudo apt-get install vim vim-gtk
 
 
 
