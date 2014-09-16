2b6e488f3b5e77b12682c37275fc1642b4d965f5
liluo <mwumli@hotmail.com>
2014-09-16 15:13:49 +0800

    add a vim colorschems 'molokai' in vim/colors/

--- config.sh
+++ config.sh
@@ -5,6 +5,9 @@ sudo apt-get install git
 # 下载Vundle到~/.vim/bundle/Vundle.vim
 mkdir -p ~/.vim/bundle/ &&
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir -p ~/.vim/colors &&
+cp vim/colors/molokai.vim ~/.vim/colors/
+
 mv vim/.vimrc ~/.vimrc		"移动.vimrc到你的home目录
 
 
