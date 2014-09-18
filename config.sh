fe9131a201f62be364cef84446efdcd6221c4adf
liluo <mwumli@hotmail.com>
2014-09-18 10:29:41 +0800

    新添.vim/config.sh:vim配置脚本

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # 更新软件源
 sudo apt-get update 
-# n安装基本编译环境  
+# 安装基本编译环境  
 sudo apt-get install build-essential
 # 安装ctags
 sudo apt-get install ctags
@@ -11,16 +11,18 @@ sudo apt-get install git
 # 下载Vundle到~/.vim/bundle/Vundle.vim
 mkdir -p ~/.vim/bundle/ &&
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# vim配色插件
-mkdir -p ~/.vim/colors &&
-cp vim/colors/molokai.vim ~/.vim/colors/
-
 # 生成基本头文件的tags
 cd /usr/include/ && sudo ctags ./*.h && cd - && cd /usr/include/c++/ && sudo ctags -R --c++-kinds=+p --fields=+ialS --extra=+q . && cd -
 
-cp vim/.vimrc ~/.vimrc		#移动.vimrc到你的home目录
-# 安装vim
-sudo apt-get install vim vim-gtk
+# 配置vim 
+/usr/bash ./vim/config.sh
+if [ $? -eq 0 ]; then 
+	echo "Vim配置成功"
+else
+	echo "vim配置失败"
+fi
+
+
 
 
 
---
+++ vim/config.sh
@@ -0,0 +1,95 @@
+#!/bin/bash 
+
+# create~/.vim/bundle
+if mkdir -p ~/.vim/bundle >/dev/null ; then 
+	echo "~/.vim/bundle 已存在"
+	exit 1
+else
+	echo "~/.vim/bundle 创建成功"
+fi 
+
+# clone plugin Vundle into ~/bundle/
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ $? -eq 0 ]; then 
+	echo "Vim管理插件Vundle安装成功"
+else
+	echo "Vim管理插件Vundle安装失败"
+	echo "退出安装"
+	exit 2
+fi
+
+# build vim config file
+cp ./.vimrc ~/.vimrc 
+if [ $? -eq 0 ]; then 
+	echo "vim配置文件.Vimrc安装完毕"
+else
+	echo "vim配置文件.Vimrc安装失败"
+	echo "退出安装"
+	exit 3
+fi
+
+# build vim colorschem
+# create ~/.vim/colors
+if mkdir -p ~/.vim/colors >/dev/null ; then 
+	echo "~/.vim/colors 已存在"
+else
+	echo "~/.vim/colors 创建成功"
+fi
+cp ./colors/molokai.vim ~/.vim/colors/ 
+if [ $? -eq 0]; then 
+	echo "配色方案构建成功"
+	echo "如果你不喜欢，请你使用colorschem自定义"
+	echo "Linux内建scheme位于:/usr/share/vim/vim74/colors"
+else
+	echo "配色方案构建失败"  
+fi
+
+# create ~/.vim/doc 
+if mkdir -p ~/.vim/doc >/dev/null ; then 
+	echo "~/.vim/doc 已存在"
+else
+	echo "~/.vim/doc 创建成功"
+fi
+
+
+# create ~/.vim/plugin 
+if mkdir -p ~/.vim/plugin >/dev/null ; then 
+	echo "~/.vim/plugin 已存在"
+else
+	echo "~/.vim/plugin 创建成功"
+fi
+
+# create ~/.vim/syntax 
+if mkdir -p ~/.vim/syntax >/dev/null ; then 
+	echo "~/.vim/syntax 已存在"
+else
+	echo "~/.vim/syntax 创建成功"
+fi
+
+# create ~/.vim/autoload
+if mkdir -p ~/.vim/autoload >/dev/null ; then 
+	echo "~/.vim/autoload 已存在"
+else
+	echo "~/.vim/autoload 创建成功"
+fi
+
+# create ~/.vim/ftplugin
+if mkdir -p ~/.vim/ftplugin >/dev/null ; then 
+	echo "~/.vim/ftplugin 已存在"
+else
+	echo "~/.vim/ftplugin 创建成功"
+fi
+# build simplified Chinese help document
+tar -xvf ./vimcdoc-1.9.0.tar.gz && cd vimcdoc-1.9.0 && ./vimcdoc.sh 
+if [ $? -eq 0 ] ; then 
+	echo "中文帮助文档建立完毕"
+	echo "你可以在vim中通过',h'开启中文/英文文档"
+else
+	echo "中文帮助文档建立失败"
+fi
+
+echo "至此，vim 配置完毕，请你在首次进入vim，切换到command模式"
+echo "输入PluginInstall"
+echo "等待命令行出现done!的字样，那么，恭喜你了，可以使用vim了"  
+
+exit 0
